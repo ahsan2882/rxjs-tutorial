a7a6416e132dfa7868468ce42b1425678df7bab8
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-15 21:29:26 +0500

    Using map with pipes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { of, from } from "rxjs";
+import { map } from 'rxjs/operators'
 
-const observable = from([1,2,3,4,5])
+const observable = of(1, 2, 3, 4, 5)
+const numbersWithSymbol = observable.pipe(
+    map((value) => {
+        `$${value}`
+    })
+)
 console.log('before')
 const subscription = observable.subscribe({
-    next(value){
+    next(value) {
         console.log(value)
     },
     complete() {

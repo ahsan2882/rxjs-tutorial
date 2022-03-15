36d554b794b965a35fbf13217d6f7fec1179e533
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-15 20:36:50 +0500

    Fixing memory leaks

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { Observable } = require("rxjs");
+
+const observable = new Observable((subscriber) => {
+    const id = setInterval(() => {
+        subscriber.next('test')
+        console.log('leak')
+    }, 1000)
+
+    subscriber.complete()
+
+    return () => {
+        clearInterval(id);
+    }
+});
+console.log('before')
+observable.subscribe({
+    next: (value) => {
+        console.log(value)
+    },
+    complete: () => {
+        console.log('Completed')
+    },
+    error: (err) => {
+        console.error(err)
+    }
+})
+console.log('after')

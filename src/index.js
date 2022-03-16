96e43725bc780dbdbd8458b23e55e91c0dcccca9
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-16 11:05:49 +0500

    Using tap operator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { interval } from "rxjs";
-import { reduce, take, scan } from 'rxjs/operators'
+import { reduce, take, scan, tap } from 'rxjs/operators'
 
 const observable = interval(500).pipe(
     take(5),
-    scan((acc, value) => acc + value, 0)
+    tap({
+        next(val) {
+            console.log(val)
+        }
+    }),
+    reduce((acc, value) => acc + value, 0)
 )
 console.log('before')
 const subscription = observable.subscribe({

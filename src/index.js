f5a2602e3a57faa90747df152dec387002254ef9
Restyled.io <commits@restyled.io>
2024-01-11 14:59:18 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-
-const {Observable} = require("rxjs");
+const { Observable } = require("rxjs");
 
 const observable = new Observable((subscriber) => {
-    const id = setInterval(() => {
-        subscriber.next('test')
-        console.log('leak')
-    }, 1000)
+  const id = setInterval(() => {
+    subscriber.next("test");
+    console.log("leak");
+  }, 1000);
 
-        subscriber.complete()
+  subscriber.complete();
 
-    return () => {
-  clearInterval(id);
-    }
-
+  return () => {
+    clearInterval(id);
+  };
 });
-    console.log('before')
-    observable.subscribe({
-      next : (value) => {console.log(value)},
-      complete : () => {console.log('Completed')
-
-      },
-      error : (err) => { console.error(err) }
-    })
-    console.log('after')
+console.log("before");
+observable.subscribe({
+  next: (value) => {
+    console.log(value);
+  },
+  complete: () => {
+    console.log("Completed");
+  },
+  error: (err) => {
+    console.error(err);
+  },
+});
+console.log("after");
